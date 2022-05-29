3400484cf5ad2d26e11c78624a9c3a7055bb7e43
Orex-x <vipboy78038@gmail.com>
2022-05-29 10:10:34 +0300

    add l;ist box

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
+using AvaloniaCallCenter.Models;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
 
@@ -11,11 +13,25 @@ namespace AvaloniaCallCenter.ViewModels
         public ICommand OnClick { get; private set; }
 
         private string _title = "hello Avalonia";
-
         public string Greeting {
             get => _title;
             set => this.RaiseAndSetIfChanged(ref _title, value);
         }
+
+
+        private ObservableCollection<Message> _myItems = new ObservableCollection<Message>
+        {
+            new Message{ Title = "hui 1" },
+            new Message{ Title = "hui 2" },
+            new Message{ Title = "hui 3" }
+        };
+        public ObservableCollection<Message> MyItems
+        {
+            get => _myItems;
+            set => this.RaiseAndSetIfChanged(ref _myItems, value);
+        }
+
+
 
         public MainWindowViewModel()
         {
@@ -23,6 +39,8 @@ namespace AvaloniaCallCenter.ViewModels
             {
                 Greeting = "hello dasha";
             });
+
+           
         }
     }
 }
